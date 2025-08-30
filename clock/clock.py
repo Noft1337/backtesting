0536bd4d089dbb2ec047f7775fcba167439b7307
Noft1337 <maudri324@gmail.com>
2025-08-30 15:52:07 +0300

    feat: clock: iterate intraday using NYSE calendar

--- clock/clock.py
+++ clock/clock.py
@@ -1,6 +1,8 @@
+import pandas as pd
+import pandas_market_calendars as mcal
 from datetime import datetime, timedelta
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Union, ClassVar
 
 from utils import parse_interval, td_to_str, discard_datetime_by_interval
 from .exceptions import IntervalNotSupported
@@ -9,7 +11,12 @@ INTERVALS_ALLOWED = ["1m", "5m", "10m", "30m", "1h", "1d", "7d", "30d", "1w", "4
 INTERVALS_ALLOWED_TD = [parse_interval(i) for i in INTERVALS_ALLOWED]
 INTERVALS_REPR = ", ".join(INTERVALS_ALLOWED)
 
+MCAL_TIME_FMT = "%Y-%m-%d"
+SCHEDULE_TIME_FMT = "%Y-%m-%d %H:%M:%S"
+MINS_30 = timedelta(minutes=30)
 
+
+# pylint: disable=R0902
 @dataclass
 class Clock:
     """This class mimics a global clock for the backtesting environment. Each time a
@@ -20,27 +27,54 @@ class Clock:
     will be updated by ``interval`` for each iteration, storing the current time in
     ``self.time``
 
+    This class uses the US Equities calendar on NY timezone. This can not be modified
+
     Args:
         start(datetime): the "epoch" of the clock, since when this clock provides time
             data
-        ival(Union[str, timedelta]): the time interval that the clock is able to support
-            as of now, only 1 interval is allowed.
+        end(datetime): The end of the date range. Default is now
+        interval(Union[str, timedelta]): the time interval that the clock is able to
+            support. as of now, only 1 interval is allowed.
             Allowed intervals: 1m, 5m, 10m, 30m, 1h, 1d, 7d, 30d, 1w, 4w
+        extended(bool): enable pre & post market times. Default is False
     """
 
     start: datetime
     end: datetime = datetime.now()
     interval: Union[str, timedelta] = "1d"
+    extended: bool = False
 
     time: datetime = field(init=False)
     _ival_str: str = field(default_factory=str, init=False)
     _ival_td: timedelta = field(default_factory=timedelta, init=False)
+
+    # Calendar Class attributes
+    tz: ClassVar[str] = "America/New_York"
+    calendar: ClassVar[str] = "NYSE"
+    nyse: ClassVar[mcal.MarketCalendar] = mcal.get_calendar(calendar)
 
     def __post_init__(self):
         self._parse_interval()
         self.time = self.start
         # Discard useless fields to be able to stop iterating
         self.end = discard_datetime_by_interval(self.end, self._ival_td)
+        self.is_intraday = self._ival_td < timedelta(days=1)
+
+        # Set up a schedule of the trading sessions
+        self._market_start = "pre" if self.extended else "market_open"
+        self._market_end = "post" if self.extended else "market_close"
+        self.sched = Clock.nyse.schedule(
+            tz=self.tz,
+            start_date=self.start.strftime(MCAL_TIME_FMT),
+            end_date=self.end.strftime(MCAL_TIME_FMT),
+            start=self._market_start,
+            end=self._market_end,
+        )
+        self._sched_i = 0
+        self._sched_len = len(self.sched)
+        self._sched_calc = True
+        self._today_open: datetime
+        self._today_close: datetime
 
     def _parse_interval(self):
         if isinstance(self.interval, str):
@@ -55,13 +89,55 @@ class Clock:
                 f"supported intervals are: {INTERVALS_REPR}"
             )
 
+    @property
+    def today_open(self):
+        if self._sched_calc:
+            loc = self.sched.columns.get_loc(self._market_start)
+            self._today_open = self.sched.iat[self._sched_i, loc].to_pydatetime()
+        return self._today_open
+
+    @property
+    def today_close(self):
+        if self._sched_calc:
+            loc = self.sched.columns.get_loc(self._market_end)
+            self._today_close = self.sched.iat[self._sched_i, loc].to_pydatetime()
+        return self._today_close
+
     def __iter__(self):
         return self
 
     def __next__(self) -> datetime:
-        if self.time > self.end:
+        if self._sched_i == self._sched_len:
             raise StopIteration
+
+        # If not intraday, open & close times shouldn't be a factor
+        if not self.is_intraday:
+            try:
+                return self.time
+            finally:
+                self.time += self._ival_td
+                self._sched_i += 1
+
+        # On intraday, we are iterating over the times within the day using
+        # ``self.interval``. this requires to factor in the open & close times and
+        # change them once we finish iterating over the current trading day.
+
+        # NOTE: this is necessary because panda_market_calendars doesn't support
+        # intraday frequencies
+        if self._sched_calc:
+            self.time = self.today_open
+
         try:
             return self.time
         finally:
-            self.time += self._ival_td
+            # On 1h chart, the last bar is only 30 mins
+            if self._ival_str == "1h" and self.time == self.today_close - MINS_30:
+                self.time += timedelta(minutes=30)
+            else:
+                self.time += self._ival_td
+                # Reached close time, recalculate open & close time
+                if self.time > self.today_close:
+                    self._sched_calc = True
+                    self._sched_i += 1
+                else:
+                    self._sched_calc = False
--- sketch/__main__.py
+++ sketch/__main__.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from models.price_action import YClient
+from models.client import YClient
+from clock import Clock
 
 CASH = 1000.0
 
 
-def main():
+def smas_cross():
     c = YClient()
     start = datetime.strptime("2025-01-01", "%Y-%m-%d")
     end = datetime.now()
@@ -55,6 +56,17 @@ def main():
     )
 
     return pa
+
+
+def iter_clock():
+    start = datetime.strptime("2025-08-01", "%Y-%m-%d")
+    c = Clock(start, interval="1h")
+    for t in c:
+        print(t.strftime("%Y-%m-%dT%H:%M:%S"))
+
+
+def main():
+    iter_clock()
 
 
 if __name__ == "__main__":

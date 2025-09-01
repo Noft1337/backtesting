e9818cf7ad93dbd404d7f74c02c709656c01e45d
Noft1337 <maudri324@gmail.com>
2025-09-01 15:10:32 +0300

    bar: added __str__ and __repr__

--- models/bar.py
+++ models/bar.py
@@ -1,3 +1,4 @@
+from consts import TIME_FMT_FULL
 from datetime import datetime
 from typing import NamedTuple
 
@@ -5,3 +6,10 @@ from typing import NamedTuple
 class Bar(NamedTuple):
     open: datetime
     close: datetime
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self):
+        fmt = TIME_FMT_FULL
+        return f"open={self.open.strftime(fmt)} close={self.close.strftime(fmt)}"

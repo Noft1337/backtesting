d0f24048c428cffb1a14bd37c93330afcd4a2343
Noft1337 <maudri324@gmail.com>
2025-09-01 15:10:15 +0300

    add: consts

---
+++ consts/__init__.py
@@ -0,0 +1,7 @@
+from .consts import *
+
+__all__ = [
+    "TIME_FMT_DAY",
+    "TIME_FMT_FULL",
+    "TIME_FMT_MINS",
+]
---
+++ consts/consts.py
@@ -0,0 +1,3 @@
+TIME_FMT_DAY = "%Y-%m-%d"
+TIME_FMT_FULL = "%Y-%m-%dT%H:%M:%S"
+TIME_FMT_MINS = "%Y-%m-%dT%H:%M:%S"
